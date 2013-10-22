8a5ca821e9f187dcebbe4a64eb464e1185c2f9b2
Denis Anfertev <danfertev@gmail.com>
2013-10-22 17:40:26 +0400

    Some improvements and bug fixes

--- src/main/scala/Sorted.scala
+++ src/main/scala/Sorted.scala
@@ -6,25 +6,28 @@ import scala.concurrent.ExecutionContext.Implicits.global
  * 10/21/13 4:26 PM
  */
 object Sorted {
-  def isSorted[A](xs: Seq[A], ord: (A, A) => Boolean): Future[Boolean] = {
-    type Indexes = (Int, Int)
-    def slice: Seq[Indexes] = {
-      val factor = xs.size / 16
-      if (factor < 2) Seq((0, xs.size - 1))
+  def isSortedPar(xs: Array[Int], ord: (Int, Int) => Boolean): Future[Boolean] = {
+    val size = xs.size
+    type Index = (Int, Int)
+    def slice: Array[Index] = {
+      val factor = size / 32
+      if (factor < 2) Array((0, size - 2))
       else {
-        val ix = 0 until xs.size - 1 by factor
-        (if (ix.last != xs.size - 1) ix :+ (xs.size - 1) else ix).sliding(2).toSeq.map(i => (i(0), i(1)))
+        val ix = 0 until size - 1 by factor
+        (if (ix.last != size - 2 || ix.last == size - 1) ix :+ (size - 2) else ix).sliding(2).toArray.map(i => (i(0), i(1)))
       }
     }
 
-    def isSliceSorted(ix: Indexes) = {
+    def isSliceSorted(ix: Index) = {
       val (start, end) = ix
       val n = end - start
-      def step(k: Int): Boolean = (k == 0) || (ord(xs(k - 1), xs(k)) && step(k - 1))
-      (n < 2) || step(n)
+
+      def step(k: Int): Boolean = (k == 0) || (ord(xs(k + start - 1), xs(k + start)) && step(k - 1))
+
+      (n < 2) || step(n + 1)
     }
 
-    def par(indexes: Seq[Indexes]): Seq[Future[Boolean]] = {
+    def par(indexes: Array[Index]): Array[Future[Boolean]] = {
       indexes map {
         ix => Future(isSliceSorted(ix))
       }
@@ -32,7 +35,7 @@ object Sorted {
 
     val result = par(slice)
 
-    def track(fs: Seq[Future[Boolean]]): Future[Boolean] = {
+    def track(fs: Array[Future[Boolean]]): Future[Boolean] = {
       if (fs.isEmpty) Future(true)
       else {
         val first = Future.firstCompletedOf(result)
@@ -43,5 +46,4 @@ object Sorted {
     }
     track(result)
   }
-
 }
--- src/test/scala/SortedSpec.scala
+++ src/test/scala/SortedSpec.scala
@@ -10,32 +10,38 @@ import scala.concurrent.Await
  */
 @RunWith(classOf[JUnitRunner])
 class SortedSpec extends Specification {
+
   import scala.concurrent.duration._
+
   val duration = Duration(30, "sec")
 
   import Sorted._
+
   "Array of Int" should {
 
     "be sorted ascending" in {
-      Await.result(isSorted[Int](Array(1, 2, 3, 3, 5), _ - _ <= 0), duration)
+      Await.result(isSortedPar(Array(1, 2, 3, 3, 5), _ - _ <= 0), duration)
     }
     "not be sorted ascending" in {
-      !Await.result(isSorted[Int](Array(1, 2, 2, 5, 4), _ - _ <= 0), duration)
+      !Await.result(isSortedPar(Array(1, 2, 2, 5, 4), _ - _ <= 0), duration)
     }
     "be sorted descending" in {
-      Await.result(isSorted[Int](Array(5, 3, 3, 2, 1), _ - _ >= 0), duration)
+      Await.result(isSortedPar(Array(5, 3, 3, 2, 1), _ - _ >= 0), duration)
     }
     "not be sorted descending" in {
-      !Await.result(isSorted[Int](Array(3, 5, 3, 2, 1), _ - _ >= 0), duration)
+      !Await.result(isSortedPar(Array(3, 5, 3, 2, 1), _ - _ >= 0), duration)
     }
     "with no element should be sorted" in {
-      Await.result(isSorted[Int](Array[Int](), _ - _ >= 0), duration)
+      Await.result(isSortedPar(Array[Int](), _ - _ >= 0), duration)
     }
     "with one element should be sorted" in {
-      Await.result(isSorted[Int](Array(1), _ - _ < 0), duration)
+      Await.result(isSortedPar(Array(1), _ - _ < 0), duration)
     }
     "with 500k elements should be sorted ascending" in {
-      Await.result(isSorted[Int]((1 to 500000).toArray, _ - _ < 0), duration)
+      Await.result(isSortedPar((1 to 500000).toArray, _ - _ < 0), duration)
+    }
+    "with 10kk elements should not be sorted ascending" in {
+      !Await.result(isSortedPar((0 to 9999999).map(x => if (x == 625000) 0 else x).toArray, _ - _ < 0), duration)
     }
   }
 }
